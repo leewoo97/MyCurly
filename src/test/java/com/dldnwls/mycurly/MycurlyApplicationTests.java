4be8da6695cbd43a36f1c11922780888d69a7a23
leewoo <dldnwls009@gmail.com>
2024-11-09 10:29:00 +0900

    TEST: 젠킨스 작동 테스트 진행 중

--- src/test/java/com/dldnwls/mycurly/MycurlyApplicationTests.java
+++ src/test/java/com/dldnwls/mycurly/MycurlyApplicationTests.java
@@ -2,8 +2,10 @@ package com.dldnwls.mycurly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")  // 프로파일 명시적으로 지정
 class MycurlyApplicationTests {
 
     @Test
