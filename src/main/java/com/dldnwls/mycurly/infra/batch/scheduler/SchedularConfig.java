07a2a561f2d02e520df2fd8f1278a63de4f63dd6
leewoo <dldnwls009@gmail.com>
2024-11-10 14:01:22 +0900

    TEST: 젠킨스 작동 테스트 진행 중(SpringBatch 중복작업 방지 설정)

--- src/main/java/com/dldnwls/mycurly/infra/batch/scheduler/SchedularConfig.java
+++ src/main/java/com/dldnwls/mycurly/infra/batch/scheduler/SchedularConfig.java
@@ -2,7 +2,9 @@ package com.dldnwls.mycurly.infra.batch.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,12 +15,26 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class SchedularConfig {
 
     private final JobLauncher jobLauncher;
+    private final JobRepository jobRepository;
     private final Job seleniumCrawlerJob;
 
     @Scheduled(cron = "0 */2 * * * *") //2분간격으로 실행
     public void runJob() {
         try {
-            jobLauncher.run(seleniumCrawlerJob, new org.springframework.batch.core.JobParameters());
+            // 이미 실행된 JobInstance가 있는지 확인
+            boolean jobInstanceExists = jobRepository.isJobInstanceExists(
+                    "seleniumCrawlerJob",
+                    new JobParameters()
+            );
+
+            if (jobInstanceExists) {
+                System.out.println("Job is already running or completed. Skipping execution.");
+                return; // 실행 중이거나 완료된 Job이 있다면 실행하지 않음
+            }
+
+            // Job 실행
+            jobLauncher.run(seleniumCrawlerJob, new JobParameters());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
